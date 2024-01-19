089cb382621249187dc54484198ad9b9e517b8ba
Dan Nadler <142226474+dannadlerp@users.noreply.github.com>
2024-01-18 20:50:24 -0500

    Update index.js
    added belongstomany, and hasone syntax to link models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,3 +5,10 @@ const Workout = require("./Workout");
 const User = require("./User");
 const ProfilePic = require("./ProfilePic");
 
+User.hasOne(City);        // USER has one CITY
+User.hasOne(Gym);            // USER has one GYM
+User.hasOne(Workout);        // USER has one WORKOUT
+User.hasOne(ProfilePic);     // USER has one PROFILEPIC
+User.belongsToMany(Gym, { through: 'UserGym', foreignKey: 'user_id' }); // GYM belongs to many USER, UserGym auto generated when using belongstoMany
+User.belongsToMany(Workout, { through: 'UserWorkout', foreignKey: 'user_id' }); // WORKOUT belongs to many USER, UserWorkout auto generated when using belongstoMany
+
