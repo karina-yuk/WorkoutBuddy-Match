ffb0e5a1c75b902638017a6f7433dbe32aa682a3
unknown <karina.yukting.li@gmail.com>
2024-01-22 21:51:30 -0500

    Created associations and exported models

--- models/index.js
+++ models/index.js
@@ -1,7 +1,29 @@
 // import models
-const City = require("./City");
-const Gym = require("./Gym");
-const Workout = require("./Workout");
-const User = require("./User");
 const ProfilePic = require("./ProfilePic");
+const User = require("./User");
+const Gym = require("./Gym");
+const City = require("./City");
+const Workout = require("./Workout");
 
+ProfilePic.belongsTo(User, {
+    foreignKey: "user_id",
+    as: "profile_pic",
+});
+
+Gym.hasMany(User, {
+    foreignKey: "gym_id",
+    as: "gym_users",
+});
+
+Workout.hasMany(User, {
+    foreignKey: "workout_id",
+    as: "workout_users",
+});
+
+User.hasOne(City, {
+    foreignKey: "city_id",
+    as: "city",
+});
+
+
+module.exports = { City, Gym, Workout, User, ProfilePic };
