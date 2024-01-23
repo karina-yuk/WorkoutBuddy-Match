2b7afa2d99422b1e3ea4699f112be8a5d426c45a
unknown <karina.yukting.li@gmail.com>
2024-01-23 16:40:01 -0500

    Created signup, login and logout post request in userRoutes.js. Required express and expoted router in homeRoutes.js

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -38,4 +38,16 @@ router.post('/login', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+// The `/api/user/logout` endpoint - Logout users
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
 module.exports = router;
