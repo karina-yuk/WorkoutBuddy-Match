8ac686ddb1140453a36180749622eea44c9047bd
Jasmine Lee <jasminelee_0409@hotmail.com>
2024-01-24 21:33:56 -0500

    Modified backend and got the root route working

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,4 +1,27 @@
 const router = require('express').Router();
-const {  } = require('../../models');
+
+
+router.get('/', async (req, res) => {
+    try {
+    //   // Get all projects and JOIN with user data
+    //   const projectData = await Project.findAll({
+    //     include: [
+    //       {
+    //         model: User,
+    //         attributes: ['username'],
+    //       },
+    //     ],
+    //   });
+    //   // Serialize data so the template can read it
+    //   const projects = projectData.map((project) => project.get({ plain: true }));
+      // Pass serialized data and session flag into template
+      res.render('landingpage', {
+        // projects,
+        logged_in: req.session.logged_in
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 module.exports = router;
