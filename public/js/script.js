b6fd767e09483042ddeaaffa5b247329f634bb5b
CYCBrian <143226784+CYCBrian@users.noreply.github.com>
2024-01-25 20:38:43 -0500

    10-FRONTEND-SIGNOUT-functionality-on-frontend

--- public/js/script.js
+++ public/js/script.js
@@ -187,22 +187,6 @@ const editGymHandler = async (event) => {
   }
 };
 
-const signoutHandler = async () => {
-  // Make a POST request to destroy the session on the back end
-  const response = await fetch('/api/users/signout', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-  });
-
-  if (response.ok) {
-    // If successfully logged out, redirect to the login page
-    console.log('Logged out successfully');
-    document.location.replace('/signin');
-  } else {
-    alert(response.statusText);
-  }
-};
-
 // Function to handle delete account
 async function deleteAccountHandler() {
 
@@ -230,8 +214,6 @@ async function deleteAccountHandler() {
     }
   }
 }
-
-
 
 document
   .querySelector('.signin-form')
@@ -266,5 +248,3 @@ document
   .addEventListener("submit", editGymHandler);
 
 document.getElementById('delete-account').addEventListener('click', deleteAccountHandler);
-
-document.getElementById('signout').addEventListener('click', signoutHandler);
