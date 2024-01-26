0853afbeec85d9a0dfd298e107d73f2fab27f47e
Dan Nadler <dan.nadlerp@gmail.com>
2024-01-25 21:46:01 -0500

    updated for PUT calls to api/profile

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 
-
 router.get('/', async (req, res) => {
     try {
     //   // Get all projects and JOIN with user data
@@ -22,6 +21,31 @@ router.get('/', async (req, res) => {
     } catch (err) {
       res.status(500).json(err);
     }
+  });
+
+  router.put('/api/profile', async (req, res) => {
+    try {
+      const { usernameEdit, emailEdit, cityEdit, workoutEdit, gymEdit } = req.body;
+      if (usernameEdit) {
+        await profile.update({username: usernameEdit}, { where: { Id: req.user.id } }); //assign to ID in  table
+      }
+      if (emailEdit) {
+        await profile.update({emai: emailEdit}, { where: { Id: req.user.id } });   //assign to ID in  table
+      }
+      if (cityEdit) {
+        await profile.update({city: cityEdit}, { where: { Id: req.city.id } }); //assign to ID in  table
+      }
+      if (workoutEdit) {
+        await profile.update({workout: workoutEdit}, { where: { Id: req.workout.id } }); //assign to ID in  table
+      }
+      if (gymEdit) {
+        await profile.update({username: gymEdit}, { where: { Id: req.gym.id } }); //assign to ID in  table
+      }
+      res.status(200).json({ message: 'update success' });
+    } catch (error) {
+        console.error('error:', error);
+    }
+
   });
 
 module.exports = router;
